68d87b8ed8f672be041bdfeeb6f5c9c5f11ecd14
EugenPotapov <peugen973@gmail.com>
2023-06-04 03:50:30 -0700

    Project added

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 13676;
+        short onemile = 20;
+        int quantity = ticket / onemile;
+
+        System.out.println(quantity);
+
+    }
+}
